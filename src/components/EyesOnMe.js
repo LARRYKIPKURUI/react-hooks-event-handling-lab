d3a15960ce944511722d0e79af1319a61b61cd0f
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-04-10 23:24:31 +0300

    Learne d about React Events

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function focusOnMe() {
+    console.log('Good!')
+}
+function blurOnMe() {
+    console.log('Hey! Eyes on me!')
+}
+
+function EyesOnMe () {
+    return (    
+        <button onFocus={focusOnMe} onBlur={blurOnMe}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
+import React from "react";
+
+function inputPass() {
+    console.log('Entering password...')
+}
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={inputPass}/>
+        </div>
     )
 }
 
